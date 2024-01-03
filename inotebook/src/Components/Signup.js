0f7c27fcd85d37d074ec154ed1b0b145e0ec919f
Ritik-Jaiswal-179 <124776083+Ritik-Jaiswal-179@users.noreply.github.com>
2024-01-03 21:40:25 +0530

    Update backend url in Signup.js

--- inotebook/src/Components/Signup.js
+++ inotebook/src/Components/Signup.js
@@ -8,7 +8,7 @@ const Signup = (props) => {
   const handlesummit = async (e) => {
     e.preventDefault()
     const { name, email, password } = credentials
-    const response = await fetch("http://localhost:4000/api/auth/createuser", {
+    const response = await fetch("https://todo-serverside-rj.onrender.com/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
